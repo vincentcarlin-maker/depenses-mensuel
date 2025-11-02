ed3d3f604f867cd7ad232b20dc1f678170eead00
vincentcarlin-maker <vincent.carlin@sfr.fr>
2025-11-02 23:02:17 +0100

    feat: Update Vite base path for GitHub Pages
    Change the Vite `base` configuration from `/depenses-mensuel/` to `./` to ensure compatibility with deployments on GitHub Pages using relative paths.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/depenses-mensuel/', // Spécifier le nom du dépôt pour le déploiement sur GitHub Pages
+  base: './', // Utiliser un chemin relatif pour une meilleure compatibilité avec GitHub Pages
 })
